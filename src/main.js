f8c5bf0f5864be13c75b65599c0d0494b76f32ed
max_shirokov <maksim.shirokov2011@mail.ru>
2021-09-28 23:01:26 +0300

    with howler and other modules

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import {printOutput} from "./output.js";
 import getDatesDiff from "./getDatesDiff.js";
 import {printResult} from "./output.js";
+import {initField, start, stop} from "./timer.js";
+import chooseFunc from "./chooseModule.js";
+
+let sound = new Howl({
+    src: 'https://assets.codepen.io/21542/howler-push.mp3'
+})
+
+sound.play()
 
 const form  = document.getElementById("datecalc");
 
@@ -20,3 +28,17 @@ form.onsubmit = (event) => {
     printResult(dateDiff);
 
 }
+
+const formTimer = initField('timer');
+
+formTimer.onsubmit = (e) => {
+    e.preventDefault();
+}
+
+formTimer.addEventListener('click', (e) => {
+    if(e.target.className !== 'start') return;
+    start(formTimer.timer)
+
+})
+
+chooseFunc('chooseForm');
